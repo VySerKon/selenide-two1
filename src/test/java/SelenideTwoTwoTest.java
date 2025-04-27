724263bcd144f4f49741be60478df200e9d2b9d5
Slava_Konovalov <slavakonovalov26@yandec.ru>
2025-04-27 16:51:29 +0300

    all

---
+++ src/test/java/SelenideTwoTwoTest.java
@@ -0,0 +1,44 @@
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.DragAndDropOptions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byTagAndText;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class SelenideTwoTwoTest {
+
+    @BeforeEach
+    public void setUp () {
+        Configuration.pageLoadStrategy = "eager";
+    }
+
+    @Test
+    public void searchTheAIPoweredDeveloperPlatformTest () {
+        open("https://github.com/");
+        $(".d-lg-flex.list-style-none").$(byText("Solutions")).hover();
+        $(byTagAndText("a", "Enterprises")).click();
+        $("#hero-section-brand-heading").shouldHave(text("The AI-powered"));
+    }
+
+    @Test
+    public void dragAndDropActionTest () {
+        open("https://the-internet.herokuapp.com/drag_and_drop");
+        $("#column-a").shouldHave(text("A"));
+        $("#column-b").shouldHave(text("B"));
+        actions().clickAndHold($("#column-a")).moveToElement($("#column-b")).release().perform();
+        $("#column-a").shouldHave(text("B"));
+        $("#column-b").shouldHave(text("A"));
+    }
+
+    @Test
+    public void dragAndDropTest () {
+        open("https://the-internet.herokuapp.com/drag_and_drop");
+        $("#column-a").shouldHave(text("A"));
+        $("#column-b").shouldHave(text("B"));
+        $("#column-a").dragAndDrop(DragAndDropOptions.to($("#column-b")));
+    }
+}
